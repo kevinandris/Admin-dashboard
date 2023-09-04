c08bece5b58ee05107aaab7ba83fe089bd446c38
keipy27 <kevinandris27@gmail.com>
2023-09-05 09:19:51 +1200

    barChartBox.tsx and its scss are completed

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -14,11 +14,12 @@ const BarChartBox = (props: Props) => {
         <h1>{props.title}</h1>
 
         <div className="chart">
-            <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={props.chartData}>
+            <ResponsiveContainer width="99%" height={150}>
+                <BarChart data={props.chartData}>
                     <Tooltip 
                         contentStyle={{background:  "#2a3447", borderRadius: "5px"}}
                         labelStyle={{display: "none"}}
+                        cursor={{fill:"none"}}
                     />
                     <Bar dataKey={props.dataKey} fill={props.color} />
                 </BarChart>
