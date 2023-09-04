ea1ebc92fb8346e63e6e0b3b54ddbe732e078a20
keipy27 <kevinandris27@gmail.com>
2023-09-05 11:27:28 +1200

    dataTableJsx, its css and users's css are also implemented

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,70 @@
+import './dataTable.scss'
+import { DataGrid, GridColDef, GridValueGetterParams, GridToolbar } from '@mui/x-data-grid'
+import { Link } from 'react-router-dom'
+
+type Props = {
+    columns: GridColDef[],
+    rows: object[];
+    slug: string;
+}
+
+const DataTable = (props: Props) => {
+
+    const handleDelete = (id:number) => {
+        // delete the item
+        // axios.delete(`/api/${slug}/id`)
+        console.log(id + " has been deleted!")
+
+    }
+    
+    const actionColumn: GridColDef = {
+        field: "action",
+        headerName: "Action",
+        width: 200,
+        renderCell: (params) => {
+            return (
+                <div className="action">
+                    <Link to={`/${props.slug}/${params.row.id}`}>
+                        <img src="/view.svg" alt="" />
+                    </Link>
+
+                    <div className='delete'>
+                        <img src="/delete.svg" alt="" onClick={() => handleDelete(params.row.id)}/>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    return (
+        <div className='dataTable'>
+                <DataGrid
+                    className='dataGrid'
+                    rows={props.rows}
+                    columns={[...props.columns, actionColumn]}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 10,
+                            },
+                        },
+                    }}
+                    slots={{toolbar: GridToolbar}}
+                    slotProps={{
+                        toolbar: {
+                            showQuickFilter: true,
+                            quickFilterProps: {debounceMs: 500}
+                        }
+                    }}
+                    pageSizeOptions={[5]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                    disableColumnFilter
+                    disableColumnSelector
+                    disableDensitySelector
+                />
+        </div>
+    )
+}
+
+export default DataTable
